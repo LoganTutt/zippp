53411f8642fd6135ddf96e61f3c36655b542441a
Logan Tutt <log.tutt@gmail.com>
2020-02-06 19:48:35 -0500

    Added random access and bidirectional operators (#4)
    * Added random access and bidirectional operators
    
    * Fix enable_if causing segfault in GCC9.2

--- include/zippp/zip.h
+++ include/zippp/zip.h
@@ -8,30 +8,71 @@ namespace zippp
 namespace detail
 {
 template<typename Tuple, typename Func, std::size_t ... Ind>
-constexpr void foreach_tuple_impl(Tuple&& tup, Func&& func, std::integer_sequence<std::size_t, Ind...>)
+constexpr inline void foreach_tuple_impl(Tuple&& tup, Func&& func, std::integer_sequence<std::size_t, Ind...>)
 {
     using ignore = int[];
     static_cast<void>(ignore{1, (func(std::get<Ind>(std::forward<Tuple>(tup))), void(), int{})...});
 }
 
 template<typename Tuple, typename Func>
-constexpr void foreach_tuple(Tuple&& tup, Func&& func)
+constexpr inline void foreach_tuple(Tuple&& tup, Func&& func)
 {
     constexpr std::size_t N = std::tuple_size<std::remove_reference_t<Tuple>>::value;
     foreach_tuple_impl(std::forward<Tuple>(tup), std::forward<Func>(func), std::make_index_sequence<N>{});
 }
 
-template<typename ... Iters>
-class zip_iterator
+template<typename Base, typename ... Types>
+constexpr inline bool all_are_type()
 {
+    return std::conjunction<std::is_base_of<Base, Types>...>::value;
+}
+
+template<typename Target, typename ... Iters>
+constexpr inline bool all_iter_are_type()
+{
+    return all_are_type<Target, typename std::iterator_traits<Iters>::iterator_category...>();
+}
+
+template<typename ... Iters>
+using iterator_tag_type = std::conditional_t<all_iter_are_type<std::random_access_iterator_tag, Iters...>(),
+    std::random_access_iterator_tag, 
+    std::conditional_t<all_iter_are_type<std::bidirectional_iterator_tag, Iters...>(),
+        std::bidirectional_iterator_tag,
+        std::conditional_t<all_iter_are_type<std::forward_iterator_tag, Iters...>(),
+            std::forward_iterator_tag,
+            std::input_iterator_tag>>>;
+
+template<typename ... Iters>
+class zip_iterator //: public std::iterator<iterator_tag_type,T,ptrdiff_t,const T*,const T&>
+{
+public:
+    using iterator_category = iterator_tag_type<Iters...>;
+    using difference_type = std::ptrdiff_t;
+private:
+    template<typename Tag>
+    static constexpr inline bool is_tag = std::is_base_of_v<Tag, iterator_category>;
+
+    // This was originally just inline with X = part of the SFINAE for the member functions, 
+    // but a compiler bug in GCC 9.2 caused a segfault.
+    // Moving it to this using directive fixes the bug, and it compiles on MSVC, GCC 9.2, anmd GCC 7.
+    template<typename T>
+    using IterEnabler = std::enable_if_t<is_tag<T>>;
+
 public:
     zip_iterator(Iters&&... iters) : iter_tup(std::forward<Iters>(iters)...){}
 
-    auto operator++()
+    decltype(auto) operator++()
     {
         foreach_tuple(iter_tup, [](auto&& iter){++iter;});
         return *this;
     }
+    auto operator++(int)
+    {
+        auto temp = *this;
+        foreach_tuple(iter_tup, [](auto&& iter){++iter;});
+        return temp;
+    }
+
     auto operator*()
     {
         return std::apply([](auto&& ... iters){return std::make_tuple<decltype(*iters)...>(*iters...);}, iter_tup);
@@ -39,6 +80,53 @@ public:
     bool operator!=(zip_iterator<Iters...> in) const
     {
         return iter_tup != in.iter_tup;
+    }
+    bool operator==(zip_iterator<Iters...> in) const
+    {
+        return iter_tup == in.iter_tup;
+    }
+
+    // Bidirectional operations
+    template<typename T = std::bidirectional_iterator_tag, typename X = IterEnabler<T>>
+    decltype(auto) operator--()
+    {
+        foreach_tuple(iter_tup, [](auto&& iter){--iter;});
+        return *this;
+    }
+    template<typename T = std::bidirectional_iterator_tag, typename X = IterEnabler<T>>
+    auto operator--(int)
+    {
+        auto temp = *this;
+        foreach_tuple(iter_tup, [](auto&& iter){--iter;});
+        return temp;
+    }
+
+    //random access operations
+    template<typename T = std::random_access_iterator_tag, typename X = IterEnabler<T>>
+    decltype(auto) operator+=(ptrdiff_t i)
+    {
+        foreach_tuple(iter_tup, [i](auto&& iter){iter+=i;});
+        return *this;
+    }
+    template<typename T = std::random_access_iterator_tag, typename X = IterEnabler<T>>
+    auto operator+(ptrdiff_t i) const
+    {
+        auto temp = *this;
+        temp += i;
+        return temp;
+    }
+    template<typename T = std::random_access_iterator_tag, typename X = IterEnabler<T>>
+    decltype(auto) operator-=(ptrdiff_t i)
+    {
+        foreach_tuple(iter_tup, [i](auto&& iter){iter-=i;});
+        return *this;
+    }
+    template<typename T = std::random_access_iterator_tag, typename X = IterEnabler<T>>
+    auto operator-(ptrdiff_t i) const
+    {
+        auto temp = *this;
+        temp -= i;
+        return temp;
     }
 
 private:
